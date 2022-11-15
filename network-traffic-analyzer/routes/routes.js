caf843de6f8097d27ba591f1fb49b339b77ee518
RASMI9601 <118123001+RASMI9601@users.noreply.github.com>
2022-11-15 20:56:01 +0530

    Add files via upload

---
+++ network-traffic-analyzer/routes/routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const banController = require('../controllers/Ban.controller');
+const DetectController = require('../controllers/detect.Controller');
+const LoginController = require('../controllers/login.controller');
+const isAuth = require('../middleware/isAuth');
+
+app.get('/health',(req,res)=>{
+    return res.status(200).json({
+        message: "~App is live~"
+    })
+})
+app.get('/',(req,res)=>{
+    return res.redirect('/login');
+})
+
+//Login route.
+app.get('/login',LoginController.ShowLoginPage);
+app.post('/login',LoginController.ActionLoginPage);
+
+app.get('/add',isAuth.isAuthenticated,banController.Showpage);
+app.post('/addban',isAuth.isAuthenticated,banController.AddData);
+
+app.get('/detect',isAuth.isAuthenticated,DetectController.check);
+
+app.get('/logout',LoginController.logout);
+
+module.exports = app;
